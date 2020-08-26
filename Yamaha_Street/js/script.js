9b06decde60c18c12707890449677e226d8ac678
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-08-26 22:18:04 +0300

    adjusting header-top and header-bottom

--- Yamaha_Street/js/script.js
+++ Yamaha_Street/js/script.js
@@ -312,27 +312,3 @@ window.addEventListener("scroll", changeActiveClassOnScroll); */
   // ibg();
    //resetForm();
 }); 
-
-/*var mySwiper = new Swiper('.slider-1', {
-   autoplay: {
-         delay: 4000,
-         disableOnInteraction: false
-      },
-   loop: true,
-   slidesPerView: 1,
-   spaceBetween: 10,
-     breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      }
-}) */
